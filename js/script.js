89556220b07a12faf46f8c803f9d2c52309a320a
Daviidvl <davidovalee@icloud.com>
2025-05-05 19:22:59 -0300

    2.0

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById('navMobile');
-  const botao = document.getElementById('hamburguer');
-
-  botao.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-  });
